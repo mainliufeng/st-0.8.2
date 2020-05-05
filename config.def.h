bc802f67dd671c0eb762f2e94438e1695f8532c1
刘峰 <mainliufeng@gmail.com>
2020-05-05 21:01:32 +0800

    change font to hack

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Menlo for Powerline:size=10:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
